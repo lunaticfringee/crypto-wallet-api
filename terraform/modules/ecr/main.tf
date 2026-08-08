101a5313292a6ab3db267b33bd23dea7ba154b91
lunatic fringe <pradeeplunatic@gmail.com>
2026-08-08 21:02:35 +0900

    Add ECR lifecycle policies: expire untagged images after 7 days, keep last 10 tagged

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -13,3 +13,39 @@ resource "aws_ecr_repository" "repos" {
     Environment = var.environment
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "repos" {
+  for_each   = aws_ecr_repository.repos
+  repository = each.value.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+        description  = "Expire untagged images after 7 days"
+        selection = {
+          tagStatus   = "untagged"
+          countType   = "sinceImagePushed"
+          countUnit   = "days"
+          countNumber = 7
+        }
+        action = {
+          type = "expire"
+        }
+      },
+      {
+        rulePriority = 2
+        description  = "Keep only the last 10 tagged images"
+        selection = {
+          tagStatus      = "tagged"
+          tagPatternList = ["*"]
+          countType      = "imageCountMoreThan"
+          countNumber    = 10
+        }
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
