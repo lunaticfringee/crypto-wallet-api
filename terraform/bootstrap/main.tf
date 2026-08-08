bf82f8456fe1dac882b3e2cdbba401c6cec52ba0
lunatic fringe <pradeeplunatic@gmail.com>
2026-08-08 13:32:13 +0900

    Add S3 state bucket permissions for GitHub Actions Terraform CI

--- terraform/bootstrap/main.tf
+++ terraform/bootstrap/main.tf
@@ -130,3 +130,30 @@ resource "aws_iam_role_policy_attachment" "github_actions_readonly" {
   role       = aws_iam_role.github_actions.name
   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
+
+resource "aws_iam_role_policy" "github_actions_state_access" {
+  name = "github-actions-terraform-state-access"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "arn:aws:s3:::crypto-wallet-api-terraform-state-798404182966/*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = "arn:aws:s3:::crypto-wallet-api-terraform-state-798404182966"
+      }
+    ]
+  })
+}
